aa92e7669001b0578acdc88db8e802e2a9c8a312
Nic Jackson <jackson.nic@gmail.com>
2019-10-17 15:14:52 +0100

    Updated install script

--- docs/install.sh
+++ docs/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+VERSION=0.1.1
 
 echo "Installing Consul K8s"
 
 echo "Copy Assets"
 wget https://github.com/nicholasjackson/consul-k8s-dev/archive/v0.1.0.tar.gz -O /tmp/consul-k8s.tar.gz
-tar -zxf /tmp/consul-k8s.tar.gz
+tar -zxf /tmp/consul-k8s.tar.gz -C /tmp/
 
-mkdir $HOME/.consul_k8s
+echo "Remove existing install"
+rm -rf $HOME/.consul_k8s
+rm -f /usr/local/bin/consul-k8s-dev
 
-mv /tmp/consul-k8s/install/* $HOME/.consul_k8s
-ln -s $HOME/.consul_k8s/consul-k8s /usr/local/bin
+echo "Copying config"
+mv /tmp/consul-k8s-dev-$VERSION/install $HOME/.consul_k8s
+ln -s $HOME/.consul_k8s/consul-k8s-dev /usr/local/bin
