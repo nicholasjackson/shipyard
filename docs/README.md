37f289f95d056b2c5a460a53f10c43adbf4a46c9
Nic Jackson <jackson.nic@gmail.com>
2019-10-17 15:05:41 +0100

    Update install script

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,9 @@
 # Consul K8s Dev
 Run Consul and Kubernetes locally in Docker
+
+## Installation
+To install run the following command in your terminal:
+
+```
+curl http://consul-k8s.demo.gs/install.sh | bash
+```
--- docs/install.sh
+++ docs/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Installing Consul K8s"
+
+echo "Copy Assets"
+wget https://github.com/nicholasjackson/consul-k8s-dev/archive/v0.1.0.tar.gz -O /tmp/consul-k8s.tar.gz
+tar -zxf /tmp/consul-k8s.tar.gz
+
+mkdir $HOME/.consul_k8s
+
+mv /tmp/consul-k8s/install/* $HOME/.consul_k8s
+ln -s $HOME/.consul_k8s/consul-k8s /usr/local/bin
